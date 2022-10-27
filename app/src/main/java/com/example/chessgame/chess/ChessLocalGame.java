3e332fadde4a276353944bfb789a6f35b3b7a5ac
basestations <santosme23@up.edu>
2022-10-27 14:15:23 -0700

    ChessMainActivity added + added to ChessLocalGame

--- app/src/main/java/com/example/chessgame/chess/ChessLocalGame.java
+++ app/src/main/java/com/example/chessgame/chess/ChessLocalGame.java
@@ -3,8 +3,24 @@ package com.example.chessgame.chess;
 import com.example.chessgame.GameFramework.LocalGame;
 import com.example.chessgame.GameFramework.actionMessage.GameAction;
 import com.example.chessgame.GameFramework.players.GamePlayer;
+import com.example.chessgame.chess.infoMessage.ChessState;
 
 public class ChessLocalGame extends LocalGame {
+
+    //constructor
+    public ChessLocalGame() {
+        // perform superclass initialization
+        super();
+        // create a new, unfilled-in TTTState object
+        super.state = new ChessState();
+    }
+
+    //constructor with loaded chessState
+    public ChessLocalGame(ChessState chessState){
+        super();
+        super.state = new ChessState(chessState);
+    }
+
     @Override
     protected void sendUpdatedStateTo(GamePlayer p) {
 
--- app/src/main/java/com/example/chessgame/chess/ChessMainActivity.java
+++ app/src/main/java/com/example/chessgame/chess/ChessMainActivity.java
@@ -1,25 +1,75 @@
 package com.example.chessgame.chess;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.example.chessgame.GameFramework.GameMainActivity;
 import com.example.chessgame.GameFramework.LocalGame;
 import com.example.chessgame.GameFramework.gameConfiguration.GameConfig;
+import com.example.chessgame.GameFramework.gameConfiguration.GamePlayerType;
 import com.example.chessgame.GameFramework.infoMessage.GameState;
+import com.example.chessgame.GameFramework.players.GamePlayer;
+import com.example.chessgame.GameFramework.utilities.Logger;
+import com.example.chessgame.GameFramework.utilities.Saving;
 import com.example.chessgame.R;
+import com.example.chessgame.chess.infoMessage.ChessState;
+import com.example.chessgame.chess.players.ChessComputerPlayer;
+import com.example.chessgame.chess.players.ChessHumanPlayer;
+
+
+import java.util.ArrayList;
 
 public class ChessMainActivity extends GameMainActivity {
+    private static final String TAG = "ChessMainActivity";
+    public static final int PORT_NUMBER = 5213;
 
-    //added a comment
     @Override
     public GameConfig createDefaultConfig() {
-        return null;
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        //Local Human Player
+        playerTypes.add(new GamePlayerType("Local Human Player") {
+            public GamePlayer createPlayer(String name) {
+                return new ChessHumanPlayer(name);
+            }
+        });
+
+        //computer player(dumb)
+        playerTypes.add(new GamePlayerType("Computer Player (dumb)") {
+            public GamePlayer createPlayer(String name) {
+                return new ChessComputerPlayer(name);
+            }
+        });
+
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2,2, "Chess", PORT_NUMBER);
+
+        // Add the default players
+        defaultConfig.addPlayer("Human", 0);
+        defaultConfig.addPlayer("Computer", 3);
+
+        // Set the initial information for the remote player
+        defaultConfig.setRemoteData("Remote Player", "", 1); // red-on-yellow GUI
+
+        return defaultConfig;
     }
 
     @Override
     public LocalGame createLocalGame(GameState gameState) {
-        return null;
+        if(gameState == null)
+            return new ChessLocalGame();
+        return new ChessLocalGame((ChessState) gameState); //copy constructor?
     }
+
+    @Override
+    public GameState saveGame(String gameName) {
+        return super.saveGame(getGameString(gameName));
+    }
+
+    @Override
+    public GameState loadGame(String gameName){
+        String appName = getGameString(gameName);
+        super.loadGame(appName);
+        Logger.log(TAG, "Loading: " + gameName);
+        return (GameState) new ChessState((ChessState) Saving.readFromFile(appName, this.getApplicationContext()));
+    }
+
 }
