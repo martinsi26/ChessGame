4ea36b10a4c736554cc11cb989ebdba7cfbc808d
martinsi26 <112525187+martinsi26@users.noreply.github.com>
2022-10-28 10:24:17 -0700

    Test

--- app/src/main/java/com/example/chessgame/chess/players/ChessHumanPlayer.java
+++ app/src/main/java/com/example/chessgame/chess/players/ChessHumanPlayer.java
@@ -92,7 +92,6 @@ public class ChessHumanPlayer extends GameHumanPlayer implements View.OnTouchLis
     protected void initAfterReady() {
         myActivity.setTitle("Chess: "+allPlayerNames[0]+" vs. "+allPlayerNames[1]);
     }
-    //
 
     /**
      * callback method when the screen it touched. We're
