e1f0a12222400555bb4815110e71a034d504bb17
Stations <77379276+substations@users.noreply.github.com>
2022-11-10 13:52:25 -0800

    Revert "added captured pieces to array"
    This reverts commit 571fdeddce7157fa87cdb2c2fbdeb46e25b78089.

--- app/src/main/java/com/example/chessgame/chess/views/WhiteCaptureSurfaceView.java
+++ app/src/main/java/com/example/chessgame/chess/views/WhiteCaptureSurfaceView.java
@@ -7,6 +7,5 @@ import com.example.chessgame.GameFramework.utilities.FlashSurfaceView;
 public class WhiteCaptureSurfaceView extends FlashSurfaceView {
     public WhiteCaptureSurfaceView(Context context) {
         super(context);
-
     }
 }
