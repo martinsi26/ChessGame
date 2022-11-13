c66b4b4d0a99096a53955ef617a2c6702298a489
martinsi26 <112525187+martinsi26@users.noreply.github.com>
2022-11-13 10:10:39 -0800

    Updated LocalGame for checking

--- app/src/main/java/com/example/chessgame/chess/pieces/Queen.java
+++ app/src/main/java/com/example/chessgame/chess/pieces/Queen.java
@@ -12,6 +12,9 @@ public class Queen {
     private ArrayList<Integer> xMovementAttack;
     private ArrayList<Integer> yMovementAttack;
 
+    private ArrayList<Integer> xAttackThrough;
+    private ArrayList<Integer> yAttackThrough;
+
     private int x;
     private int y;
 
@@ -24,6 +27,8 @@ public class Queen {
         yMovement = new ArrayList<>();
         xMovementAttack = new ArrayList<>();
         yMovementAttack = new ArrayList<>();
+        xAttackThrough = new ArrayList<>();
+        yAttackThrough = new ArrayList<>();
         if(color == Piece.ColorType.WHITE) {
             colorInverse = Piece.ColorType.BLACK;
         } else if (color == Piece.ColorType.BLACK) {
@@ -38,11 +43,11 @@ public class Queen {
     }
 
     public void diagonal(ChessState state, Piece.ColorType color) {
-
         boolean stopUpLeft = false;
         boolean stopUpRight = false;
         boolean stopDownLeft = false;
         boolean stopDownRight = false;
+        boolean stop = false;
 
         for (int i = 1; i < 8; i++) {
             if (y - i >= 0 && x - i >= 0) {
@@ -53,10 +58,19 @@ public class Queen {
                     yMovementAttack.add(y - i);
                     stopUpLeft = true;
                 }
+                if (state.getPiece(x - i, y - i).getPieceColor() == colorInverse && !stopUpLeft) {
+                    xMovementAttack.add(x - i);
+                    yMovementAttack.add(y - i);
+                }
                 if (state.getPiece(x - i, y - i).getPieceColor() == color) {
                     xMovementAttack.add(x - i);
                     yMovementAttack.add(y - i);
+                    stop = true;
                     stopUpLeft = true;
+                }
+                if (state.getPiece(x - i, y - i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x - i);
+                    yAttackThrough.add(y - i);
                 }
                 if (!stopUpLeft) {
                     xMovementAttack.add(x - i);
@@ -73,10 +87,19 @@ public class Queen {
                     yMovement.add(y - i);
                     stopUpRight = true;
                 }
+                if (state.getPiece(x + i, y - i).getPieceColor() == colorInverse && !stopUpRight) {
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y - i);
+                }
                 if (state.getPiece(x + i, y - i).getPieceColor() == color) {
                     xMovementAttack.add(x + i);
                     yMovementAttack.add(y - i);
                     stopUpRight = true;
+                    stop = true;
+                }
+                if (state.getPiece(x + i, y - i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x + i);
+                    yAttackThrough.add(y - i);
                 }
                 if (!stopUpRight) {
                     xMovementAttack.add(x + i);
@@ -93,10 +116,19 @@ public class Queen {
                     yMovement.add(y + i);
                     stopDownLeft = true;
                 }
+                if (state.getPiece(x - i, y + i).getPieceColor() == colorInverse && !stopDownLeft) {
+                    xMovementAttack.add(x - i);
+                    yMovementAttack.add(y + i);
+                }
                 if (state.getPiece(x - i, y + i).getPieceColor() == color) {
                     xMovementAttack.add(x - i);
                     yMovementAttack.add(y + i);
                     stopDownLeft = true;
+                    stop = true;
+                }
+                if (state.getPiece(x - i, y + i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x - i);
+                    yAttackThrough.add(y + i);
                 }
                 if (!stopDownLeft) {
                     xMovementAttack.add(x - i);
@@ -113,10 +145,19 @@ public class Queen {
                     yMovement.add(y + i);
                     stopDownRight = true;
                 }
+                if (state.getPiece(x + i, y + i).getPieceColor() == colorInverse && !stopDownRight) {
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y + i);
+                }
                 if (state.getPiece(x + i, y + i).getPieceColor() == color) {
                     xMovementAttack.add(x + i);
                     yMovementAttack.add(y + i);
                     stopDownRight = true;
+                    stop = true;
+                }
+                if (state.getPiece(x + i, y + i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x + i);
+                    yAttackThrough.add(y + i);
                 }
                 if (!stopDownRight) {
                     xMovementAttack.add(x + i);
@@ -133,6 +174,7 @@ public class Queen {
         boolean stopRight = false;
         boolean stopUp = false;
         boolean stopDown = false;
+        boolean stop = false;
 
         for (int i = 1; i < 8; i++) {
             if (x - i >= 0) {
@@ -143,10 +185,19 @@ public class Queen {
                     yMovementAttack.add(y);
                     stopLeft = true;
                 }
+                if (state.getPiece(x - i, y).getPieceColor() == colorInverse && !stopLeft) {
+                    xMovementAttack.add(x - i);
+                    yMovementAttack.add(y);
+                }
                 if (state.getPiece(x - i, y).getPieceColor() == color) {
                     xMovementAttack.add(x - i);
                     yMovementAttack.add(y);
                     stopLeft = true;
+                    stop = true;
+                }
+                if (state.getPiece(x - i, y).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x - i);
+                    yAttackThrough.add(y);
                 }
                 if (!stopLeft) {
                     xMovement.add(x - i);
@@ -163,10 +214,19 @@ public class Queen {
                     yMovementAttack.add(y - i);
                     stopUp = true;
                 }
+                if (state.getPiece(x, y - i).getPieceColor() == colorInverse && !stopUp) {
+                    xMovementAttack.add(x);
+                    yMovementAttack.add(y - i);
+                }
                 if (state.getPiece(x, y - i).getPieceColor() == color) {
                     xMovementAttack.add(x);
                     yMovementAttack.add(y - i);
                     stopUp = true;
+                    stop = true;
+                }
+                if (state.getPiece(x, y - i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x);
+                    yAttackThrough.add(y - i);
                 }
                 if (!stopUp) {
                     xMovement.add(x);
@@ -183,10 +243,19 @@ public class Queen {
                     yMovementAttack.add(y + i);
                     stopDown = true;
                 }
+                if (state.getPiece(x, y + i).getPieceColor() == colorInverse && !stopDown) {
+                    xMovementAttack.add(x);
+                    yMovementAttack.add(y + i);
+                }
                 if (state.getPiece(x, y + i).getPieceColor() == color) {
                     xMovementAttack.add(x);
                     yMovementAttack.add(y + i);
                     stopDown = true;
+                    stop = true;
+                }
+                if (state.getPiece(x, y + i).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x);
+                    yAttackThrough.add(y + i);
                 }
                 if (!stopDown) {
                     xMovement.add(x);
@@ -197,22 +266,31 @@ public class Queen {
             }
             if (x + i < 8) {
                 if (state.getPiece(x + i, y).getPieceColor() == colorInverse && !stopRight) {
-                    xMovementAttack.add(x);
-                    yMovementAttack.add(y + i);
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y);
                     xMovement.add(x + i);
                     yMovement.add(y);
                     stopRight = true;
                 }
+                if (state.getPiece(x + i, y).getPieceColor() == colorInverse && !stopRight) {
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y);
+                }
                 if (state.getPiece(x + i, y).getPieceColor() == color) {
-                    xMovementAttack.add(x);
-                    yMovementAttack.add(y + i);
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y);
                     stopRight = true;
+                    stop = true;
+                }
+                if (state.getPiece(x + i, y).getPieceColor() != color && !stop) {
+                    xAttackThrough.add(x + i);
+                    yAttackThrough.add(y);
                 }
                 if (!stopRight) {
                     xMovement.add(x + i);
                     yMovement.add(y);
-                    xMovementAttack.add(x);
-                    yMovementAttack.add(y + i);
+                    xMovementAttack.add(x + i);
+                    yMovementAttack.add(y);
                 }
             }
         }
@@ -232,5 +310,13 @@ public class Queen {
 
     public ArrayList<Integer> getYAttack() {
         return yMovementAttack;
+    }
+
+    public ArrayList<Integer> getXAttackThrough() {
+        return xAttackThrough;
+    }
+
+    public ArrayList<Integer> getYAttackThrough() {
+        return yAttackThrough;
     }
 }
