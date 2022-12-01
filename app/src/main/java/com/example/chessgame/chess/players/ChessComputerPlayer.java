9474309dcafbc9181a7244a9ff74b36e7cfe69eb
nerenber26 <nerenber26@up.edu>
2022-11-30 22:53:55 -0800

    Changed sleep time in comp player to make it move faster

--- app/src/main/java/com/example/chessgame/chess/players/ChessComputerPlayer.java
+++ app/src/main/java/com/example/chessgame/chess/players/ChessComputerPlayer.java
@@ -84,7 +84,7 @@ public class ChessComputerPlayer extends GameComputerPlayer {
         int xVal = selection.getX();
         int yVal = selection.getY();
         game.sendAction(new ChessSelectAction(this, xVal, yVal));
-        sleep(.2);
+        sleep(.5);
 
 
         if (selection.getPieceType() == Piece.PieceType.PAWN) {
